bf06c8af714a7ae18d8e7f84ebe8305bdb2d2ee7
Ryan Shaw <rshaw727@gmail.com>
2023-05-07 21:23:50 -0700

    update

--- blackjack.ps1
+++ blackjack.ps1
@@ -1,13 +1,33 @@
+Write-Host "-----"
 Write-Host "Welcome to Blackjack in PowerShell!"
+Write-Host "-----"
+Write-Host 'Initializing starting balance of $100...'
+Write-Host "-----"
+$balance = 100
 
 $numCards = 2..10
 $faceCards = "J", "Q", "K", "A"
 [System.Collections.ArrayList] $deck = $numCards * 4 + $faceCards * 4
 
-Write-Host 'State your bet! Minimum is $2 and maximum is $100'
-Read-Host "Enter your bet"
+Write-Host "State your bet! Minimum is `$2, maximum is `$$balance"
+Write-Host "Type in your bet below or hit Enter for default bet of `$5"
 
-$dealerCards = (Deal-Card), (Deal-Card)
+do {
+    try {
+        $bet = Read-Host "Enter your bet"
+        $bet.ToInt32()
+        if ($bet -eq "") {
+            $bet = 5
+        }
+        [int]$bet = Read-Host "Enter your bet"
+    }
+    catch {
+        Write-Host "Unable to accept input, defaulting to bet of `$5"
+    }
+    Write-Host $bet
+} until (($bet -ge 2) -and ($bet -le 100))
+
+$dealerCards = (Get-Card), (Get-Card)
 Write-Host $dealerCards
 
 function Get-Card {
